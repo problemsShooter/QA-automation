04484a90fe9ded8b84424e48017f7967aa019c68
Vitaliy <vitalii.grebnev@mail.ru>
2018-12-10 16:31:01 +0400

    log4j

--- src/test/java/selenium/tests/TestBase.java
+++ src/test/java/selenium/tests/TestBase.java
@@ -2,10 +2,7 @@ package selenium.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 
 public class TestBase {
@@ -14,27 +11,29 @@ public class TestBase {
 
     @BeforeSuite
     public void beforeSuite() {
-//        String exePath = "C:\\Set of program\\driverChrome\\chromedriver.exe";
-//        System.setProperty("webdriver.chrome.driver", exePath);
-//        driver = new ChromeDriver();
     }
 
     @BeforeMethod
     public void beforeMethod() {
-        String exePath = "C:\\JetBrains\\ChromeWebDriver\\chromedriver.exe";
+        String exePath = "C:\\Set of program\\driverChrome\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", exePath);
         driver = new ChromeDriver();
     }
 
     @AfterSuite
     public void afterSuite() {
+
+    }
+
+    @AfterMethod
+    public void afterMethod() {
         driver.quit();
         driver = null;
     }
-    @AfterMethod
-    public void afterMethod() {
-        /*driver.quit();
-        driver = null;*/
+
+    @AfterTest
+    public void afterTest() {
+
     }
 
 }
--- src/test/java/selenium/tests/TestRegistration.java
+++ src/test/java/selenium/tests/TestRegistration.java
@@ -1,6 +1,8 @@
 package selenium.tests;
 
 import models.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -9,9 +11,13 @@ import selenium.pageObject.PageShopRegistration;
 import selenium.actions.Registration;
 
 public class TestRegistration extends TestBase {
+    private static final Logger LOGGER = LogManager.getLogger(
+            TestRegistration.class.getName());
 
     @Test(dataProvider = "users", dataProviderClass = DataUsers.class)
     public void verifyUserRegistration(User user) {
+        LOGGER.debug("Debug Message Logged !!!");
+        LOGGER.info( "Info Message: verifyUserRegistration has been run !!!" );
         PageShopRegistration pageShopRegistration = PageShopRegistration.open(driver);
 
         Registration.registration(pageShopRegistration, user);
